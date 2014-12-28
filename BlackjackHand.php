f87f89b949146c8f8e7dfa88dbae0bbab89c3963
Jonathan <jonathan@thephpsolution.com>
2014-12-27 20:25:13 -0600

    Created BlackjackHand assignment

---
+++ BlackjackHand.php
@@ -0,0 +1,7 @@
+<?php
+
+// Create BlackjackHand class as a subclass of Hand
+
+// Hand will return a value of 8 when it has an Ace and a 7
+
+// Make BlackjackHand return a value of 18 when it has an Ace and a 7
